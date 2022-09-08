ecc3e7c09d6db41ba21092f37842a5faa45d3483
monse420 <90013348+monse420@users.noreply.github.com>
2022-09-07 22:22:01 -0500

    Add files via upload

---
+++ db.php
@@ -0,0 +1,5 @@
+<?php
+$conexion=msqli_conennect("localhost", "root","","validar")
+?>
+
+<h1>CONECTADO</h1>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad Integradora</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+   <form action="validar.php" method="post">
+   <h1>ZONA GAMER</h1>
+   <p> ID de Usuario <input type="text" placeholder="ingrese su ID" name="usuario"></p>
+   <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="contraseña"></p>
+   <input type="submit" value="Ingresar">
+   
+   </form> 
+</body>
+</html>
